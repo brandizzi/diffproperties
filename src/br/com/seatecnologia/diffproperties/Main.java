33783e297bfcb2264747e7aa962c25574db61675
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2011-10-18 18:46:57 -0200

    Now considers first value too - d'oh\!

--- src/br/com/seatecnologia/diffproperties/Main.java
+++ src/br/com/seatecnologia/diffproperties/Main.java
@@ -1,8 +1,10 @@
 package br.com.seatecnologia.diffproperties;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -11,16 +13,35 @@ import java.util.Properties;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			File moduleFile;
+			try {
+				moduleFile = new File(Main.class.getProtectionDomain()
+						.getCodeSource().getLocation().toURI());
+				System.err.println("Usage:\n" +
+						"    java -jar " + moduleFile.getName() + " <properties-file-1> <properties-file-2>");
+			} catch (URISyntaxException e) {
+				System.err.println("Unexpected error while getting name of jar file: "+e.getLocalizedMessage());
+			}
+			return;
+		}
 		String file1 = args[0];
 		String file2 =  args[1];
 		
 		System.out.println("Original: "+file1 + "\nCompared to: "+ file2+"\n");
 		
-		Properties p1 = loadProperties(file1), 
-		p2 = loadProperties(file2);
+		Properties p1, p2;
+		try {
+			p1 = loadProperties(file1);
+			p2 = loadProperties(file2);
+		} catch (IOException e) {
+			System.err.println("Error opening a file: " + e.getLocalizedMessage());
+			return;
+		}
 		Enumeration<String> pn1 = (Enumeration<String>) p1.propertyNames();
-		for (String key = pn1.nextElement(); pn1.hasMoreElements();	key = pn1.nextElement()) {
+		while (pn1.hasMoreElements()) {
+			String key = pn1.nextElement(); 
 			String p1value = p1.getProperty(key);
 			String p2value = p2.getProperty(key);
 			if (p2value == null) {
@@ -32,7 +53,8 @@ public class Main {
 
 		}
 		Enumeration<String> pn2 = (Enumeration<String>) p2.propertyNames();
-		for (String key = pn2.nextElement(); pn2.hasMoreElements();	key = pn2.nextElement()) {
+		while (pn2.hasMoreElements()) {
+			String key = pn2.nextElement(); 
 			String p1value = p1.getProperty(key);
 			String p2value = p2.getProperty(key);
 			if (p1value == null) {
